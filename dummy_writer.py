6309801a6e2eb8afa0b70a8c596a27153ddaec94
ginsenglee <thecynicdog0328@gmail.com>
2023-07-07 11:58:45 +0900

    inserted dictionary of entries over `hr.employees.job_id`, `hr.employees.manager_id`, `hr.employees.department_id`

--- dummy_writer.py
+++ dummy_writer.py
@@ -20,9 +20,43 @@ def dummy(id: int, job: str, manager: int, dept: int):
 
     return f"({id},'{first_name}','{last_name}','{email}','{phone}','{date}','{job}',{salary},{commission_pct},{manager},{dept})"
 
+job_manager_dept = [
+    ("IT_PROG", 102, 60),
+    ("IT_PROG", 103, 60),
+    ("FI_MGR", 101, 100),
+    ("FI_ACCOUNT", 108, 100),
+    ("PU_MAN", 100, 30),
+    ("PU_CLERK", 114, 30),
+    ("ST_MAN", 100, 50),
+    ("ST_CLERK", 120, 50),
+    ("ST_CLERK", 121, 50),
+    ("ST_CLERK", 122, 50),
+    ("ST_CLERK", 123, 50),
+    ("ST_CLERK", 124, 50),
+    ("SA_MAN", 100, 80),
+    ("SA_REP", 145, 80),
+    ("SA_REP", 146, 80),
+    ("SA_REP", 147, 80),
+    ("SA_REP", 148, 80),
+    ("SA_REP", 149, 80),
+    ("SH_CLERK", 120, 50),
+    ("SH_CLERK", 121, 50),
+    ("SH_CLERK", 122, 50),
+    ("SH_CLERK", 123, 50),
+    ("SH_CLERK", 124, 50),
+    ("AD_ASST", 101, 10),
+    ("MK_MAN", 100, 20),
+    ("MK_REP", 201, 20),
+    ("HR_REP", 101, 40),
+    ("PR_REP", 101, 70),
+    ("AC_MGR", 101, 110),
+    ("AC_ACCOUNT", 205, 110),
+]
+
 result = []
-for i in range(300, 5000):
-    result.append(dummy(i, "ST_CLERK", 121, 50))
+for i in range(500, 55500):
+    job, manager, dept = random.choice(job_manager_dept)
+    result.append(dummy(i, job, manager, dept))
 
 insert_statement = "INSERT INTO employees (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, commission_pct, manager_id, department_id)\nVALUES\n"
 
