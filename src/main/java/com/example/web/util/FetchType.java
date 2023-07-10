841c216afd1f17116df876e6aa7983a8a7362dfb
ginsenglee <thecynicdog0328@gmail.com>
2023-07-10 09:11:27 +0900

    dropped method

--- src/main/java/com/example/web/util/FetchType.java
+++ src/main/java/com/example/web/util/FetchType.java
@@ -2,14 +2,4 @@ package com.example.web.util;
 
 public enum FetchType {
     EAGER, LAZY;
-
-    public static boolean matchesFetchType(FetchType[] fetchTypes, FetchType fetchType) {
-        for (FetchType type : fetchTypes) {
-            if (type == fetchType) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
-
