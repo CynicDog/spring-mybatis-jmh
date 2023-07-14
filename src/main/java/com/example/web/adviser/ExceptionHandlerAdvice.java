1e1130a73c1bc6d0e7c3c9d4f7994ed040be0df7
ginsenglee <thecynicdog0328@gmail.com>
2023-07-14 10:03:31 +0900

    adviser

---
+++ src/main/java/com/example/web/adviser/ExceptionHandlerAdvice.java
@@ -0,0 +1,36 @@
+package com.example.web.adviser;
+
+import org.jboss.logging.Logger;
+import org.springframework.dao.DataAccessException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    private Logger logger = Logger.getLogger(ExceptionHandlerAdvice.class);
+
+    @ExceptionHandler({ Exception.class })
+    public String handleException(Exception e) {
+
+        logger.error(e.getMessage(), e);
+
+        return "error/unknown";
+    }
+
+    @ExceptionHandler({ RuntimeException.class })
+    public String handleException(RuntimeException e) {
+
+        logger.error(e.getMessage(), e);
+
+        return "error/server";
+    }
+
+    @ExceptionHandler({ DataAccessException.class })
+    public String handleException(DataAccessException e) {
+
+        logger.error(e.getMessage(), e);
+
+        return "error/database";
+    }
+}
